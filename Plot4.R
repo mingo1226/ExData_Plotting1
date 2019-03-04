d09b5401c04970407308ea798e14c5dfe22f6685
Andrew Leonard <46637131+mingo1226@users.noreply.github.com>
2019-03-04 09:28:54 -0600

    Update Plot4.R

--- Plot4.R
+++ Plot4.R
@@ -1,8 +1,6 @@
 library(lubridate)
 
 #read in data
-fileUrl <-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
-download.file(fileUrl, destfile = "household_power_consumption.text", mentod ="curl")
 data <- read.table("household_power_consumption.txt", header=TRUE, stringsAsFactors=FALSE, sep=";")
 
 #subset data for correct dates
